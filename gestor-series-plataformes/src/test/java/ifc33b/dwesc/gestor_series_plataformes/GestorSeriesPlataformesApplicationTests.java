40d5d15ca9d5ace2ef1a0b3f5b9ff17350326264
Cristian Pérez <cperezjuarez@cifpfbmoll.eu>
2026-02-02 21:09:46 +0100

    Fix data-postgresql.sql

--- gestor-series-plataformes/src/test/java/ifc33b/dwesc/gestor_series_plataformes/GestorSeriesPlataformesApplicationTests.java
+++ gestor-series-plataformes/src/test/java/ifc33b/dwesc/gestor_series_plataformes/GestorSeriesPlataformesApplicationTests.java
@@ -2,8 +2,10 @@ package ifc33b.dwesc.gestor_series_plataformes;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class GestorSeriesPlataformesApplicationTests {
 
 	@Test
